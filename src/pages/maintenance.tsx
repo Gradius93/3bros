25c689abf73c1c63f5c9109133b45e100d565992
Sean Gray <sean.gray@applydigital.com>
2026-01-16 21:08:42 +0000

    3BROS-2 maintenance cover

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import styles from "./maintenance.module.css";
+
 export default function Maintenance() {
   return (
-    <div>
-      <h1>Maintenance</h1>
+    <div className={styles.container}>
+      {/* Mobile Image */}
+      <Image
+        src="/images/3bros_mobile_safe_1080x1920.png"
+        alt="3 Bros Logo"
+        fill
+        style={{ objectFit: "cover" }}
+        priority
+        className={styles.mobileImage}
+      />
+      {/* Tablet Image */}
+      <Image
+        src="/images/3Bros_Website_Landing_Page.png"
+        alt="3 Bros Logo"
+        fill
+        style={{ objectFit: "cover" }}
+        priority
+        className={styles.tabletImage}
+      />
+      {/* Desktop Image */}
+      <Image
+        src="/images/3bros_desktop_safe_1920x1080.png"
+        alt="3 Bros Logo"
+        fill
+        style={{ objectFit: "cover" }}
+        priority
+        className={styles.desktopImage}
+      />
     </div>
   );
 }
